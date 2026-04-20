c9bc389ca2da7fad33d66dd70f447c9a81195cf0
codebasea <110478608+codebasea@users.noreply.github.com>
2026-04-21 00:02:42 +0530

    Entity Class added

---
+++ src/main/java/oracledb/webapp/api/controller/EmployeeService.java
@@ -0,0 +1,33 @@
+package oracledb.webapp.api.controller;
+
+import oracledb.webapp.api.entity.Employee;
+import oracledb.webapp.api.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository repository;
+
+    public EmployeeService(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Employee> getAll() {
+        return repository.findAll();
+    }
+
+    public Employee save(Employee employee) {
+        return repository.save(employee);
+    }
+
+    public Employee getById(Long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
